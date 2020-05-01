d980714d8f8add107be2a545b4d70dcebad6dd28
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-05-01 13:41:57 -0400

    tests 2

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,13 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
+    fn calculate_apple_price(price: i64) -> bool {
+        true
+    }
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(true, calculate_apple_price(35));
     }
 }
